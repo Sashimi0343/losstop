c4961d1a7aff428874a2914ab3371d41fdc16705
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-13 09:13:38 +0900

    トップページに商品の一覧を表示

--- app/controllers/bazaars_controller.rb
+++ app/controllers/bazaars_controller.rb
@@ -1,6 +1,8 @@
 class BazaarsController < ApplicationController
+  before_action :set_bazaar, only: [:show, :edit, :update, :destroy]
 
   def index
+    @bazaars = Bazaar.all
   end
 
   def new
@@ -36,6 +38,10 @@ class BazaarsController < ApplicationController
   def bazaar_params
     params.require(:bazaar).permit(:image, :title, :description, :price, :stock, :deadline,
                                    :category_id, :delivery_charge_id, :days_to_ship_id).merge(company_id: current_company.id)
+  end
+
+  def set_bazaar
+    @bazaar = Bazaar.find(params[:id])
   end
 
 end
