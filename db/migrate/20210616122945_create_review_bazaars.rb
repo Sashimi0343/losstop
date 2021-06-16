d7ee7da7ca24b988236b4d303f91733e5fab4380
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-16 23:41:52 +0900

    マイグレーションファイル編集

---
+++ db/migrate/20210616122945_create_review_bazaars.rb
@@ -0,0 +1,12 @@
+class CreateReviewBazaars < ActiveRecord::Migration[6.0]
+  def change
+    create_table :review_bazaars do |t|
+
+      t.text :comment, null: false
+      t.references :bazaar, null: false, foreign_key: true
+      t.references :company, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
