f4d2e983e8a9acc98078ee033d26ee7bcaad2f96
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-19 02:51:43 +0900

    コントローラー編集

--- app/controllers/contact_auctions_controller.rb
+++ app/controllers/contact_auctions_controller.rb
@@ -1,2 +1,31 @@
 class ContactAuctionsController < ApplicationController
+  before_action :check_login, only: %i[create]
+
+  def create
+    contact_auction = ContactAuction.new(contact_params)
+
+    if current_company == nil && (current_user.id != contact_auction.user.id)
+      redirect_to root_path
+    elsif current_user == nil && (current_company.id != contact_auction.company.id)
+      redirect_to root_path
+    else
+      contact_auction.save
+    end
+  end
+
+  private
+
+  def contact_params
+    if current_user == nil
+      params.require(:contact_auction).permit(:message).merge(order_auction_id: params[:order_auction_id], company_id: current_company.id)
+    else
+      params.require(:contact_auction).permit(:message).merge(order_auction_id: params[:order_auction_id], user_id: current_user.id)
+    end
+  end
+
+  def check_login
+    unless user_signed_in? || company_signed_in?
+      redirect_to root_path
+    end
+  end
 end
