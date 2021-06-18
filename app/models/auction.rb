4b204e67da0fac68de2b970d2a95f86cb9d5bc12
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-18 23:19:22 +0900

    マイグレーション・モデル設定

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,25 @@
 class Auction < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :title, :description
+
+    validates :price, numericality: { only_integer: true, message: 'は半角数字で入力してください。' }
+    validates_inclusion_of :price, in: 300..999_999_999, message: 'は300円から設定可能です。'
+
+    validates :stock, numericality: { only_integer: true, message: 'は半角数字で入力してください。' }
+    validates_inclusion_of :stock, in: 1..99_999, message: 'は1〜99,999個まで設定可能です。'
+  end
+
+  with_options numericality: { other_than: 0, message: 'を選択してください。' } do
+    validates :category_id, :delivery_charge_id, :days_to_ship_id
+  end
+
+  belongs_to :category
+  belongs_to :days_to_ship
+  belongs_to :delivery_charge
+
+  #has_many :order_auctions
+  #has_many :review_auctions
+  belongs_to :user
 end
--- db/migrate/20210618133314_create_auctions.rb
+++ db/migrate/20210618133314_create_auctions.rb
@@ -2,6 +2,14 @@ class CreateAuctions < ActiveRecord::Migration[6.0]
   def change
     create_table :auctions do |t|
 
+      t.string     :title,               null: false
+      t.text       :description,         null: false
+      t.integer    :price,               null: false
+      t.integer    :stock,               null: false
+      t.integer    :category_id,         null: false
+      t.integer    :delivery_charge_id,  null: false
+      t.integer    :days_to_ship_id,     null: false
+      t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
   end
