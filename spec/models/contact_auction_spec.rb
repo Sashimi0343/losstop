5d8fcef6ba9a7d85ef71e693e6d558892fa80d91
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-19 03:13:41 +0900

    単体テスト実装

--- spec/models/contact_auction_spec.rb
+++ spec/models/contact_auction_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe ContactAuction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    company = FactoryBot.create(:company)
+    order = FactoryBot.create(:order_auction)
+    @contact = FactoryBot.build(:contact_auction, company_id: company.id, user_id: user.id, order_auction_id: order.id, )
+  end
+
+  describe '取引連絡のメッセージについて' do
+    context 'OK：取引メッセージが送信できる○' do
+      it 'OK：全ての項目が適切に記入出来ていればメッセージが送信できる。' do
+        expect(@contact).to be_valid
+        sleep 1
+      end
+
+      it 'OK：companyが空でも良い' do
+        @contact.company_id = nil
+        expect(@contact).to be_valid
+        sleep 1
+      end
+
+      it 'OK：userが空でも良い' do
+        @contact.user_id = nil
+        expect(@contact).to be_valid
+        sleep 1
+      end
+    end
+    context 'NG：取引メッセージが送信できない×' do
+      it 'NG：messageが空' do
+        @contact.message = ""
+        @contact.valid?
+        expect(@contact.errors.full_messages).to include("メッセージを入力してください")
+        sleep 1
+      end
+
+      it 'NG：order_auction_idが存在しない' do
+        @contact.order_auction_id = nil
+        @contact.valid?
+        expect(@contact.errors.full_messages).to include("Order auctionを入力してください")
+        sleep 1
+      end
+    end
+  end
 end
