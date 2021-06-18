ecc2558cc6217796bc4da64f5d479a547330eadf
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-19 02:51:27 +0900

    マイグレーションファイル編集

--- db/migrate/20210618171159_create_contact_auctions.rb
+++ db/migrate/20210618171159_create_contact_auctions.rb
@@ -2,6 +2,10 @@ class CreateContactAuctions < ActiveRecord::Migration[6.0]
   def change
     create_table :contact_auctions do |t|
 
+      t.text :message, null: false
+      t.references :order_auction, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :company, foreign_key: true
       t.timestamps
     end
   end
