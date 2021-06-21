cb00caf3406cd83dfca20a9459f514d9c56df2cb
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-22 03:03:06 +0900

    コントローラー改修

--- app/controllers/review_auctions_controller.rb
+++ app/controllers/review_auctions_controller.rb
@@ -9,9 +9,9 @@ class ReviewAuctionsController < ApplicationController
 
   def review_params
     if current_user == nil
-      params.require(:review_auction).permit(:comment).merge(auction_id: params[:auction_id], company_id: current_company.id)
+      params.require(:review_auction).permit(:comment).merge(name: current_company.name, auction_id: params[:auction_id], company_id: current_company.id)
     else
-      params.require(:review_auction).permit(:comment).merge(auction_id: params[:auction_id], user_id: current_user.id)
+      params.require(:review_auction).permit(:comment).merge(name: current_user.nickname, auction_id: params[:auction_id], user_id: current_user.id)
     end
   end
 
