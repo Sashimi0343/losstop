0d98edec195ee9a658acc46e38d66032ba3920b8
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-26 06:04:30 +0900

    company completed

---
+++ app/javascript/new_company.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if ( document.getElementById('new_company')){
+    const ImageBazaar = document.getElementById('image_company');
+
+    document.getElementById('company_image').addEventListener('change', function(e){
+      const createImageHTML = (imageURL) => {
+
+        const text = document.getElementById('text_company')
+        const divElement = document.createElement('div');
+        const image = document.createElement('img');
+        image.setAttribute('src', imageURL);
+
+        divElement.appendChild(image);
+        ImageBazaar.appendChild(divElement);
+        text.remove();
+      };
+
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const imageURL = window.URL.createObjectURL(file);
+
+      createImageHTML(imageURL);
+    });
+  }
+});
