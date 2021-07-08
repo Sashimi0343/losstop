75be19d3ff2aa530651b515525f40f9533d5d5c3
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-07-09 04:46:43 +0900

    arranged order_bazaar_show

--- app/models/order_bazaar.rb
+++ app/models/order_bazaar.rb
@@ -1,12 +1,13 @@
 class OrderBazaar < ApplicationRecord
   with_options presence: true do
-    validates :bazaar_id, :user_id
+    validates :bazaar_id
     validates :quantity, numericality: { only_integer: true, message: 'は半角数字で入力してください。' }
   end
 
   has_many :contact_bazaars, dependent: :destroy
   belongs_to :bazaar
   belongs_to :user
+  belongs_to :company
 
   has_many :notice_companys, dependent: :destroy
 end
