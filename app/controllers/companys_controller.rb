70ac7fc60a3aabaa4391818d89616ddb81387882
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-07-13 04:35:36 +0900

    arranged company my page

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -2,7 +2,8 @@ class CompanysController < ApplicationController
   def show
     @company = Company.find(params[:id])
     @bazaars = @company.bazaars
-    @orders = @company.order_bazaars
+    @order_bazaars = @company.order_bazaars
+    @order_auctions = @company.order_auctions
   end
 
   def edit; end
