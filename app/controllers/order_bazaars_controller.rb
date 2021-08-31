ad9070053eca015cda081a772f3071b71e657bfd
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-08-31 20:52:00 +0900

    completed order_bazaar

--- app/controllers/order_bazaars_controller.rb
+++ app/controllers/order_bazaars_controller.rb
@@ -1,5 +1,5 @@
 class OrderBazaarsController < ApplicationController
-  before_action :set_bazaar, only: %i[new create]
+  before_action :set_bazaar, only: %i[new create destroy]
   before_action :authenticate_user!, only: %i[new create]
 
   def new
@@ -33,10 +33,17 @@ class OrderBazaarsController < ApplicationController
     end
   end
 
+  def destroy
+    @order = OrderBazaar.find(params[:bazaar_id])
+    @order.update(active: "close")
+    redirect_to bazaars_path
+  end
+
   private
 
   def order_bazaar_params
-    params.require(:order_bazaar).permit(:quantity).merge(user_id: current_user.id, company_id: @bazaar.company.id, bazaar_id: @bazaar.id)
+    params.require(:order_bazaar).permit(:quantity).merge(
+                    active: "active", user_id: current_user.id, company_id: @bazaar.company.id, bazaar_id: @bazaar.id)
   end
 
   def set_bazaar
--- db/migrate/20210614133221_create_contact_bazaars.rb
+++ db/migrate/20210614133221_create_contact_bazaars.rb
@@ -3,7 +3,6 @@ class CreateContactBazaars < ActiveRecord::Migration[6.0]
     create_table :contact_bazaars do |t|
       t.string     :name,           null: false
       t.text       :message,        null: false
-      t.boolean    :active,         nill: false
       t.references :order_bazaar,   null: false, foreign_key: true
       t.references :company,        foreign_key: true
       t.references :user,           foreign_key: true
